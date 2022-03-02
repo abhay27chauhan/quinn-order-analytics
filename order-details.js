a8a5bfd69125c42582b70780fe0bef3bbae91606
Abhay Chauhan <chauhan27.abhay@gmail.com>
2022-03-02 17:18:25 +0530

    some changes

--- order-details.js
+++ order-details.js
@@ -32,7 +32,7 @@ const getStore = () => {
 
 const getStartDate = () => {
   return new Promise((resolve, reject) => {
-    reader.question("Start Date (DD-MM-YYYY) ", (answer) => {
+    reader.question("Start Date (ISO 8601) ", (answer) => {
       resolve(answer);
     });
   });
@@ -40,7 +40,7 @@ const getStartDate = () => {
 
 const getEndDate = () => {
   return new Promise((resolve, reject) => {
-    reader.question("End Date (DD-MM-YYYY) ", (answer) => {
+    reader.question("End Date (ISO 8601) ", (answer) => {
       resolve(answer);
     });
   });
@@ -51,26 +51,6 @@ const main = async () => {
   const STORE = (await getStore()) || STORE_VARS["STORE"];
   const startDate = (await getStartDate()) || STORE_VARS["startDate"];
   const endDate = (await getEndDate()) || STORE_VARS["endDate"];
-
-  const startDateArray = startDate.split("-");
-  const endDateArray = endDate.split("-");
-
-  let created_at_min;
-  let created_at_max;
-  if (startDateArray.length == 3) {
-    created_at_min = new Date(
-      startDateArray[2],
-      startDateArray[1],
-      startDateArray[0]
-    ).toISOString();
-  }
-  if (endDateArray.length == 3) {
-    created_at_max = new Date(
-      endDateArray[2],
-      endDateArray[1],
-      endDateArray[0]
-    ).toISOString();
-  }
 
   axiosApiInstance.interceptors.request.use(
     async (config) => {
@@ -103,49 +83,51 @@ const main = async () => {
     },
   };
 
-  const direct = new Set();
-  const indirect = new Set();
-  const others = new Set();
+  const direct = [];
+  const indirect = [];
+  const others = [];
 
   async function call(store, path, last) {
     let reqUrl = `https://${store}/admin/api/2021-10/${path}.json?limit=250`;
     reqUrl = last == 0 ? reqUrl : `${reqUrl}&since_id=${last}`;
     reqUrl =
-      created_at_min == undefined
+      !startDate
         ? reqUrl
-        : `${reqUrl}&created_at_min=${created_at_min}`;
+        : `${reqUrl}&created_at_min=${startDate}`;
     reqUrl =
-      created_at_max == undefined
+      !endDate
         ? reqUrl
-        : `${reqUrl}&created_at_max=${created_at_max}`;
+        : `${reqUrl}&created_at_max=${endDate}`;
 
     console.log(reqUrl);
 
     const result = await axiosApiInstance.get(reqUrl);
     const orders = result.data.orders;
+    if (orders.length == 0 && last == 0){
+      console.log("orders ", orders);
+      return;
+    }
 
     queryData["totalOrders"] = queryData["totalOrders"] + orders.length;
 
     for (let i = 0; i < orders.length; i++) {
       const order = orders[i];
       const line_items = order["line_items"];
-      const customerId = order.customer && order.customer.id;
-      if (customerId) {
-        for (let j = 0; j < line_items.length; j++) {
-          const properties = line_items[j]["properties"];
-          if (properties.length > 0) {
-            const orderType = properties[0]["value"]["order_type"];
-            orderType == "direct" && direct.add(customerId);
-            orderType == "indirect" && indirect.add(customerId);
-            orderType == "others" && others.add(customerId);
-          }
+      for (let j = 0; j < line_items.length; j++) {
+        const properties = line_items[j]["properties"];
+        if (properties.length > 0) {
+          const orderType = properties[0]["value"]["order_type"];
+          const distinctId = properties[0]["value"]["distinct_id"]
+          orderType == "direct" && direct.push(distinctId);
+          orderType == "indirect" && indirect.push(distinctId);
+          orderType == "others" && others.push(distinctId);
         }
       }
     }
     if (orders.length < 250) {
-      queryData.distinctIds.direct = direct.size;
-      queryData.distinctIds.indirect = indirect.size;
-      queryData.distinctIds.others = others.size;
+      queryData.distinctIds.direct = direct.length;
+      queryData.distinctIds.indirect = indirect.length;
+      queryData.distinctIds.others = others.length;
       console.log(queryData);
     //   console.log("distinct ids direct", direct);
     //   console.log("distinct ids indirect", indirect);
